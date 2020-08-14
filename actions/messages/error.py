0622e2d977239990adf85cf735bca6a04125593e
Michel <michelalmeidasilva@outlook.pt>
2020-08-14 18:16:06 -0300

    added error consts

---
+++ actions/messages/error.py
@@ -0,0 +1,4 @@
+class Error(object) :
+    ESTABELECIMENTO_NOTFOUND = "Nao entendemos o nome do estabelecimento, pode falar novamente?"
+    ELETRICISTA_NOTFOUND = "Não entendemos o nome do eletricista, pode repetir a frase novamente? "
+    CATEGORIA_ESTABELECIMENTO_NOTFOUND = "não entendemos o nome dessa categoriam pode repetir a frase novamente?"
